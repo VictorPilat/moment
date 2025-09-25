43c19365f6622ca5431dbaa2a16c53e4a710ce94
Vitya <ccowes050807@gmail.com>
2025-09-25 22:44:01 +0300

    new structure

--- moment.js
+++ moment.js
@@ -5,24 +5,26 @@ const fs = require("fs")
 const app = express()
 
 
+const PORT = 8000
+const HOST = 'localhost'
+
+
 const postsPath = path.join(__dirname,"posts.json")
 const posts = JSON.parse(fs.readFileSync(postsPath,"utf-8"))
 
+
+function getDate(){
+    return moment().format("Y/MM/DD H:mm:ss");
+}
 
 
 app.get("/posts",(req,res) =>[
     res.status(200).json(posts)
 ])
-function getDate(){
-    return moment().format("Y/MM/DD H:mm:ss");
-}
-
-const PORT = 8000
-const HOST = 'localhost'
-
 app.get("/timestamp", (req, res) =>{
     res.json("" + getDate());
 })
+
 
 app.listen(PORT, HOST, () => {
     console.log(`Server started on http://${HOST}:${PORT}`)
