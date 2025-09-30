dda092f08a95c535511500962a28f4585da082d0
Vitya <ccowes050807@gmail.com>
2025-09-30 18:10:52 +0300

    added skip and take

--- moment.js
+++ moment.js
@@ -18,15 +18,51 @@ function getDate(){
 }
 
 
-app.get("/posts",(req,res) =>[
-    res.status(200).json(posts)
-])
+// app.get("/posts",(req,res) =>[
+//     res.status(200).json(posts)
+// ])
 
+app.get("/posts", (req,res)=>{
+    console.log(req.query)
+    const take = req.query.take
+    const skip = req.query.skip
+
+
+    if (take && skip) {
+        if (isNaN(+take) || isNaN(+skip)) {
+            res.status(400).json("is not a number")
+            return
+        }
+        const skipTake = posts.slice(+skip, 0 + +take)
+        res.status(200).json(skipTake)
+        return
+    }
+    if (take) {
+        if (isNaN(+take)){
+            res.status(400).json("is not a number")
+            return
+        }
+        const slicedPosts = posts.slice(0, +take)
+        res.status(200).json(slicedPosts)
+        return
+    }
+    if (skip) {
+        if (isNaN(+skip)){
+            res.status(400).json("is not a number")
+            return
+        }
+        const slicedPosts = posts.slice(+skip)
+        res.status(200).json(slicedPosts)
+        return
+    }
+
+    res.status(200).json(posts)
+})
 app.get("/posts/:id",(req, res)=>{
     const id = +req.params.id
     if (isNaN(id)){
         res.status(400).json("id must be an integer");
-        return;
+        return
     }
     const post = posts.find((pr)=>{
         const isMatch = pr.id === id
